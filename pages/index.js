8325ebba17489b3821b783a4ea788b9793a03f60
Nik Volynkin <niknorff@gmail.com>
2023-03-06 12:30:13 +0200

    GitHub button and warning sign change

--- pages/index.js
+++ pages/index.js
@@ -84,6 +84,10 @@ export default function Home() {
 		setTime(0);
 	};
 
+	const githubLink = () => {
+		window.open("https://github.com/niknorf/capital-gpt3-finnish-3B");
+	};
+
 	return (
 		<>
 			<Head>
@@ -104,6 +108,7 @@ export default function Home() {
 						🤗 Hugging Face:{" "}
 						<a
 							href="https://huggingface.co/TurkuNLP/gpt3-finnish-3B"
+							target="_blank"
 							className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
 						>
 							repo
@@ -179,10 +184,29 @@ export default function Home() {
 					{isLoading && (
 						<div>
 							<div className="mt-3">
-								<span className="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">
-									⚠️ The model is hosted on the free Hugging Face instance,
-									initial inference might take up to 1,5 minutes.
-								</span>
+								<div
+									className="flex p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+									role="alert"
+								>
+									<svg
+										aria-hidden="true"
+										className="flex-shrink-0 inline w-5 h-5 mr-3"
+										fill="currentColor"
+										viewBox="0 0 20 20"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+											clip-rule="evenodd"
+										></path>
+									</svg>
+									<span className="sr-only">Info</span>
+									<div>
+										The model is hosted on the free Hugging Face instance. The
+										initial non-cached inference might take up to 1,5 minutes.
+									</div>
+								</div>
 							</div>
 							<div role="status" className="mt-3 flex items-center">
 								<svg
@@ -222,6 +246,30 @@ export default function Home() {
 							<span> 📍{answer.generated_text}</span>
 						</div>
 					)}
+					<div className="fixed bottom-0 right-0 transform translate-x-2">
+						<button
+							type="button"
+							onClick={githubLink}
+							className="text-white mb-7 mr-9 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30"
+						>
+							<svg
+								className="w-4 h-4 mr-2 -ml-1"
+								aria-hidden="true"
+								focusable="false"
+								data-prefix="fab"
+								data-icon="github"
+								role="img"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 496 512"
+							>
+								<path
+									fill="currentColor"
+									d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+								></path>
+							</svg>
+							Source code on GitHub
+						</button>
+					</div>
 				</div>
 			</main>
 		</>
